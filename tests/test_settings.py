38eeeb71c6ae0abbfb0bfc65728b1492bd7b397d
Can Mustafa Özdemir <canmustafaozdemir@gmail.com>
2016-01-20 11:09:32 +0200

    .travis.yml added

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,16 +1,17 @@
 import os
+import dj_database_url
 
 BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.pardir)
 
 SECRET_KEY = 'not_so_secret_r2fetCebUAZB3DhzTn5NPg8J08IancuGt04npTMh'
 DEBUG = True
 
+db_url = os.environ.get("DATABASE_URL",
+                        "postgres://postgres@127.0.0.1/django_nece_test")
+DB = dj_database_url.parse(db_url)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nece',
-        'USER': 'postgres',
-    }
+    'default': DB
 }
 
 INSTALLED_APPS = ('nece', 'tests',)

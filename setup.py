545b5efe293064ddce8e5b921e4bacc3157f4377
Can Mustafa Özdemir <canmustafaozdemir@gmail.com>
2016-01-22 22:01:27 +0200

    setup.py improved

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-description = "A content translation framework using Postgresql's " +\
-    "jsonb field in the background",
+description = "A content translation framework using Postgresql's jsonb" + \
+    " field in the background",
 
 version = '0.4.1'
 url = 'https://github.com/tatterdemalion/django-nece'
 download_url = '/'.join([url, 'tarball', version])
 
+with open('README.rst', 'rb') as f:
+    long_description = f.read().decode('utf-8')
+
+
 setup(
     name='nece',
     version=version,
     description=description,
-    long_description=description,
+    long_description=long_description,
     author='Can Mustafa Özdemir',
     author_email='canmustafaozdemir@gmail.com',
     url=url,
@@ -24,6 +28,7 @@ setup(
         'Django>=1.8',
         'psycopg2>=2.5.4'
     ],
+    license='BSD License',
     classifiers=[
         "Framework :: Django",
         "Intended Audience :: Developers",

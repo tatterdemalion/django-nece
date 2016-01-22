19acad36484e769c27a2c137e6b88ae191d5347d
Can Mustafa Özdemir <canmustafaozdemir@gmail.com>
2016-01-22 14:19:44 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,15 +4,19 @@ from setuptools import setup
 description = "A content translation framework using Postgresql's " +\
     "jsonb field in the background",
 
+version = '0.4'
+url = 'https://github.com/tatterdemalion/django-nece'
+download_url = '/'.join([url, 'tarball', version])
+
 setup(
     name='nece',
-    version='0.3',
+    version=version,
     description=description,
     long_description=description,
     author='Can Mustafa Özdemir',
     author_email='canmustafaozdemir@gmail.com',
-    url='https://github.com/tatterdemalion/django-nece',
-    download_url='https://github.com/tatterdemalion/django-nece/tarball/0.3',
+    url=url,
+    download_url=download_url,
     keywords=['translations', 'i18n', 'language', 'multilingual'],
     packages=['nece', 'nece.fields', 'nece.fields.pgjson',
               'nece.fields.pgjson.forms'],

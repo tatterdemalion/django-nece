b50640354d79fd132f3419573d33d51e92893b07
Can Mustafa Ozdemir <canmustafaozdemir@gmail.com>
2017-08-21 12:14:33 +0300

    django1.11 tests

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
-
+from pip.req import parse_requirements
 
 with open('README.rst') as f:
     ld = f.read()
 
 long_description = ld.replace(ld[0:ld.find('nece?')], '')
 
-version = '0.6.1'
+version = '0.7.0'
 description = "A content translation framework using Postgresql's jsonb" + \
     " field in the background"
 url = 'https://github.com/tatterdemalion/django-nece'
 download_url = '/'.join([url, 'tarball', version])
+
+install_reqs = parse_requirements('requirements.txt')
+
 
 setup(
     name='nece',
@@ -24,10 +27,7 @@ setup(
     download_url=download_url,
     keywords=['translations', 'i18n', 'language', 'multilingual'],
     packages=['nece'],
-    install_requires=[
-        'Django>=1.9',
-        'psycopg2>=2.5.4'
-    ],
+    install_requires = [str(ir.req) for ir in install_reqs],
     license='BSD License',
     classifiers=[
         "Framework :: Django",

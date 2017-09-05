948ddb282a3baf3e5c297f8baaa70e348379048a
Can Mustafa Ozdemir <canmustafaozdemir@gmail.com>
2017-09-05 11:27:03 +0300

    version bump with a fix

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
+
 with open('README.rst') as f:
     ld = f.read()
 
 long_description = ld.replace(ld[0:ld.find('nece?')], '')
 
-version = '0.7.1'
+version = '0.7.2'
 description = "A content translation framework using Postgresql's jsonb" + \
     " field in the background"
 url = 'https://github.com/tatterdemalion/django-nece'
 download_url = '/'.join([url, 'tarball', version])
-
-with open('requirements.txt') as f:
-    install_requires = f.read().splitlines()
 
 setup(
     name='nece',
@@ -26,7 +24,10 @@ setup(
     download_url=download_url,
     keywords=['translations', 'i18n', 'language', 'multilingual'],
     packages=['nece'],
-    install_requires=install_requires,
+    install_requires=[
+        'Django>=1.9',
+        'psycopg2>=2.5.4'
+    ],
     license='BSD License',
     classifiers=[
         "Framework :: Django",
@@ -36,7 +37,6 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Topic :: Database",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Text Processing :: Linguistic",
